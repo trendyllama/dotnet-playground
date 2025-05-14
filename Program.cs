6f4ac1d430599aeed603e8bd50d66a331648f72d
trendyllama <agog.pretzel-0o@icloud.com>
2025-05-14 23:52:09 +0000

    trying out linq

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void LinqExample()
+    {
+
+        List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        IEnumerable<string> evens = from number in numbers
+                                 where number > 5 && number % 2 == 0
+                                 orderby number descending
+                                 select $"{number}";
+
+        foreach (string even in evens)
+        {
+            Console.WriteLine(even);
+        }
+    }
+    static void Main(string[] args)
+    {
+        LinqExample();
+    }
+}
